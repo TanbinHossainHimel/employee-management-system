b3746c7ce0113e0e25b32d7ee6c989494777867f
Md Tanbin Hossain Himel <68559342+Tanbinhimel@users.noreply.github.com>
2024-01-24 23:59:03 +0600

    app layout changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
 import {Component} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import {RouterOutlet} from '@angular/router';
-import {NzIconModule} from 'ng-zorro-antd/icon';
-import {NzLayoutModule} from 'ng-zorro-antd/layout';
-import {NzMenuModule} from 'ng-zorro-antd/menu';
+import {NzIconDirective} from 'ng-zorro-antd/icon';
+import {
+  NzContentComponent,
+  NzFooterComponent,
+  NzHeaderComponent,
+  NzLayoutComponent,
+  NzSiderComponent
+} from 'ng-zorro-antd/layout';
+import {NzMenuDirective, NzMenuItemComponent} from 'ng-zorro-antd/menu';
 import {EncryptDecryptService} from "./services/encrypt-decrypt/encrypt-decrypt.service";
-import {NzButtonComponent} from "ng-zorro-antd/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule, NzButtonComponent],
+  imports: [CommonModule, RouterOutlet, NzLayoutComponent, NzSiderComponent, NzMenuDirective, NzMenuItemComponent, NzIconDirective, NzHeaderComponent, NzContentComponent, NzFooterComponent, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
@@ -22,5 +27,9 @@ export class AppComponent {
     const decryptedValue = this.encryptDecryptService.decryptWithAES256(encryptedValue);
 
     console.log(encryptedValue, decryptedValue);
+  }
+
+  toggleCollapsed(): void {
+    this.isCollapsed = !this.isCollapsed;
   }
 }
