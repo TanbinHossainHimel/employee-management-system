9275b79ff9108f121c923f7de8789ac4d5894e5b
Md Tanbin Hossain Himel <tanbin.himel@gmail.com>
2024-02-19 22:20:01 +0600

    social auth types added

--- src/app/providers/social-auth.provider.ts
+++ src/app/providers/social-auth.provider.ts
@@ -1,7 +1,8 @@
 import {GoogleLoginProvider, SocialAuthServiceConfig} from "@abacritt/angularx-social-login";
 import {environment} from "../../environments/environment";
+import {Provider} from "@angular/core";
 
-export const socialAuth = {
+export const socialAuth: Provider = {
   provide: 'SocialAuthServiceConfig',
   useValue: {
     autoLogin: false,
